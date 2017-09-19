fc8c0ab605337e0988c4959bd56258b83e65a591
Prajwal Shetty <prajwals15@gmail.com>
2017-09-18 21:34:52 -0500

    Added Description to package

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,7 +1,11 @@
-/** 
- * This is the pacakage name
+/**
+ * Domain classes used to produce the JSON and XML output for the RESTful services.
+ * <p>
+ * These classes contain the JAXB annotations.
  *
+ * @since 1.0
+ * @author jwhite
+ * @version 1.1
  */
- 
  package com.example;
-
+ 
